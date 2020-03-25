60f0712eca4ac58c94c2ffdab297edc1b7732aaa
MagicalWater <crazydennies@gmail.com>
2020-03-25 16:34:30 +0800

    * 修正全螢幕時尚未隱藏狀態欄與導航欄

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -63,6 +63,8 @@ class WebviewManager {
     private ViewGroup mMoiveParentView = null;
     private Set<Pair<Integer, Integer>> mFlags = new HashSet<>();
 
+    private int tempSystemUi;
+
     private long getFileSize(Uri fileUri) {
         Cursor returnCursor = context.getContentResolver().query(fileUri, null, null, null, null);
         returnCursor.moveToFirst();
@@ -226,6 +228,19 @@ class WebviewManager {
                 mMoiveView = view;
                 mMoiveParentView.addView(mMoiveView);
                 mMoiveParentView.setVisibility(View.VISIBLE);
+
+                View decorView = FlutterWebviewPlugin.activity.getWindow().getDecorView();
+
+                tempSystemUi = decorView.getSystemUiVisibility();
+
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
             }
 
             @Override
@@ -261,6 +276,9 @@ class WebviewManager {
                 if (webView != null) {
                     webView.setVisibility(View.VISIBLE);
                 }
+
+                View decorView = FlutterWebviewPlugin.activity.getWindow().getDecorView();
+                decorView.setSystemUiVisibility(tempSystemUi);
             }
 
             //The undocumented magic method override
